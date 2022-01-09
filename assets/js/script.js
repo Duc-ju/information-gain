eebac0002679da6a0dd7866cbc63aee2a3a65a71
Duc Ju <81551892+Duc-ju@users.noreply.github.com>
2022-01-09 15:57:48 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,26 @@ document.getElementById('tinh').onclick = () => {
 	let kem = document.getElementById('kem').value;
 	document.getElementById('ketqua').innerHTML = entropy(Number(tot), Number(kha), Number(kem))
 	document.getElementById('soluong').innerHTML = Number(tot)+Number(kha)+Number(kem);
+	document.getElementById('tot').value=''
+	document.getElementById('kha').value=''
+	document.getElementById('kem').value='';
 }
+
+document.getElementById('tinhIG').onclick = () => {
+	let h = Number(document.getElementById('h').value);
+	let i1 = Number(document.getElementById('i1').value);
+	let ni1 = Number(document.getElementById('ni1').value);
+	let i2 = Number(document.getElementById('i2').value);
+	let ni2 = Number(document.getElementById('ni2').value);
+	let i3 = Number(document.getElementById('i3').value);
+	let ni3 = Number(document.getElementById('ni3').value);
+	let res
+	let sum = ni1+ni2+ni3
+	if(!ni3){
+		document.getElementById('ig').innerHTML = h-i1*ni1/sum-i2*ni2/sum
+	}
+	else{
+		document.getElementById('ig').innerHTML = h-i1*ni1/sum-i2*ni2/sum-i3*ni3/sum
+	}
+}
+
